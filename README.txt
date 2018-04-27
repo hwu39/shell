701edc1422be53f61f55a2c5110f787d4638ac32
Hongkun Wu <hwu2@umbc.edu>
2018-04-27 18:20:22 -0400

    Fixed the shell I had in homework 1. Implemented the required built-in commands.

--- README.txt
+++ README.txt
@@ -1,6 +1,6 @@
 CMSC 421 Spring 2018
-Homework 1 Part 2
+Homework 2 Part 2
 Author: Hongkun Wu
 
 Program Description:
-This program is a simple shell program that keeps reading in user command line arguments. The program will truncate the each argument and pass the commands to exec system call. Then, the program executes all the commands entered by the user. First, the program displays a prompt to ask for user input, and gets() is used to read in each command line argument. If user enters an empty string, nothing will execute (for safety). If invalid command is entered, execvp() will return -1. and error message is displayed. Shell program then detects whether the input is "exit". If exit is entered, the program is terminated immediately. When forking is complete, shell program stops forking and waits. When commands are executed successfully, return 0.
+The shell program I have is able to run built-in commands. exit works with 0 or 1 arguments to exit the shell. If no argument is given, exit with status 0. If commands are invalid, display error. Both chdir and cd have the same functionalities. If the path is given, chdir or cd will redirect to that path. This can be checked with the command pwd. If the path is invalid, it will show the error message. If a third (or fourth, etc.) command exits, chdir or cd will simply ignore everything after the path. The echo command should unescape the given string and print it out, followed by a newline on the user's standard output. My shell passes the string to the utils file, but it only works on certain strings. And, the strings are sometimes not parsed properly.
--- simple_shell.c
+++ simple_shell.c
@@ -1,54 +1,112 @@
-#include <sys/types.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <limits.h>
+#include <sys/wait.h>
+#include "utils.h"
+
+int read_line(char *buf);
 
 int main()
 {
-pid_t pid;
-	printf("Welcome to custom shell!!! \nType your commands at the prompt. \nEnter exit to exit!\n");
-	while (1) {
-	/*fork a child process*/
-	//pid = fork();
-	//define user argument, tokenized array of commands, and temp string
-	char arg[256], *token[256],*temp;
-	printf(">>> ");
-	//check if user has entered nothing	
-	if (gets(arg) != NULL) {
-	/*fork a child process*/	
-	pid = fork();	
-	//tokenize user argument
-	temp = strtok(arg," "); 
-	int counter = 0;	
-	while (temp != NULL) {	
-		//store commands in array token	
-		token[counter] = temp;
-		counter++;
-		temp = strtok(NULL, " ");
+  pid_t pid;
+  printf("Welcome to custom shell!!! \nType your commands at the prompt. \nEnter exit to exit!\n");
+  while (1) {
+    //define user argument, tokenized array of commands, and temp string
+    char *token = malloc(sizeof(char)),*temp;
+    int size = read_line(token);
+    
+    //tokenize user argument
+    temp = strtok(token," "); 
+    int counter = 0;	
+    char *arg[size];
+    while (temp != NULL) {	
+      //store commands in array token	
+      arg[counter++] = temp;
+      temp = strtok(NULL, " ");
+    }
+
+    //replace echo functionality
+    if (strcmp(arg[0], "echo") == 0 || strcmp(arg[0], "/bin/echo") == 0){
+	FILE *errf = NULL;	
+	char *tmp;
+	tmp = unescape(arg[1],errf);	  
+	printf(tmp);
+	//skip built-in commands
+	continue;
+    }
+    /*fork a child process*/
+      pid = fork();
+    //check if user has entered anything
+    if ( size > 0) {  
+      if (pid < 0) { /* error occurred*/
+	fprintf(stderr, "Fork Failed");
+	return 1;
+      } 
+      else if (pid == 0) { /* child process */
+	//execute commands user has entered
+	if (execvp(arg[0],arg) == -1) {
+	  //invalid command returns error
+	  //perror("exec failed");
+	  return 1;
 	}
-	}	
-	if (pid < 0) { /* error occurred*/
-		fprintf(stderr, "Fork Failed");
-		return 1;
+      }
+      //if user enters exit, exit program
+      else if (strcmp(arg[0], "exit") == 0) {
+	if (arg[1] == NULL) {//single exit 
+	  exit(EXIT_SUCCESS);
+	  break;
 	}
-	else if (pid == 0) { /* child process */
-		//execute commands user has entered
-		if (execvp(token[0],token) == -1) {
-			//invalid command returns error
-			perror("exec failed");
-			return 1;
-		}
+	else if (strcmp(arg[1], "0") == 0) {//exit with 0
+	  exit(EXIT_SUCCESS);
 	}
-	//if user enters exit, exit program
-	else if(strcmp(token[0], "exit") == 0) {
-		break;
-	} 
-	else { /*parent process*/
-		/*parent will wait for the child to complete*/
-		wait(token);
-	//	printf("Child Complete\n");
+	else if (strcmp(arg[1], "1") == 0) {//exit with 1
+	  exit(EXIT_FAILURE);
 	}
+	printf("Imcompatible command with exit\n");
+      }
+      else if(strcmp(arg[0], "chdir") == 0 || strcmp(arg[0], "cd") == 0 || strcmp(arg[0], "/bin/chdir") == 0 || strcmp(arg[0], "/bin/cd") == 0){
+	//commands are ommited after the second one
+	if (arg[2] != NULL) {
+	  int i;
+	  for (i = 2; i < size; i++) {
+	    arg[i] = NULL;
+	  }
 	}
-	return 0;
+	
+	if (arg[1] == NULL) {	
+	  chdir("/home");
+	}
+	else {
+	  chdir(arg[1]);
+	}
+      }
+      else { /*parent process*/
+	/*parent will wait for the child to complete*/
+	pid = wait(NULL);
+	//printf("Child Complete\n");
+      }
+    }
+  }
+  return 0; 
+}
+
+int read_line(char *buf)
+{
+  int i, j;
+  i = 0;
+  do {
+    j = fgetc(stdin);
+    if (j == '\n' || j == EOF) {
+      return i;
+    }
+    else {
+      buf[i] = j;
+      i++;
+    }
+  } while(1);
+  return i;
 
 }
